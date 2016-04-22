dbd1bdc4727ad9951a86107a5d7dac0d3c0b0e08
Felix Li <lcfelixli@ust.hk>
2016-04-22 21:29:15 +0800

    Made the daily intake goals editable

--- app/scripts/views/nutrition_goals_view.js
+++ app/scripts/views/nutrition_goals_view.js
@@ -7,7 +7,9 @@ var app = app || {};
 		el: '.stats',
 
 		events: {
-
+			'click .btn-edit': 'edit',
+			'click .btn-submit': 'submit',
+			'click .btn-cancel': 'cancel'
 		},
 
 		initialize: function (options) {
@@ -21,12 +23,17 @@ var app = app || {};
 			this.recordedCollection = app.recordedCollection;
 
 			this.$nutritionFacts = this.$('.nutrition-facts');
+			this.$editBtn = this.$('.btn-edit');
+			this.$submitBtn = this.$('.btn-submit');
+			this.$cancelBtn = this.$('.btn-cancel');
+
 			this.$caloriesBar = this.$nutritionFacts.find('.calories .progress-bar');
 			this.$carboBar = this.$nutritionFacts.find('.carbo .progress-bar');
 			this.$fatBar = this.$nutritionFacts.find('.fat .progress-bar');
 			this.$proteinBar = this.$nutritionFacts.find('.protein .progress-bar');
 
 			this.listenTo(this.recordedCollection, 'updateSum', this.render);
+			this.listenTo(this.model, 'change', this.render);
 
 			console.log('NutritionGoalsView');
 			this.$('.bar-step').attr('style', 'padding-left: ' + this.hundredPercentLine + '%');
@@ -58,6 +65,43 @@ var app = app || {};
 				fatPercentage: this.recordedCollection.sumNutrition.sumFat / this.model.get('fat') * hundredPercentLine,
 				proteinPercentage: this.recordedCollection.sumNutrition.sumProtein / this.model.get('protein') * hundredPercentLine
 			};
+		},
+
+		edit: function () {
+			this.$nutritionFacts.find('.progress').hide();
+			this.$nutritionFacts.find('.goals-edit').show();
+
+			this.$('.calories input').val(this.model.get('calories'));
+			this.$('.carbo input').val(this.model.get('carbo'));
+			this.$('.fat input').val(this.model.get('fat'));
+			this.$('.protein input').val(this.model.get('protein'));
+
+			this.$editBtn.hide();
+			this.$cancelBtn.show();
+			this.$submitBtn.show();
+		},
+
+		submit: function () {
+			this.model.set({
+				calories: this.$('.calories input').val().trim(),
+				carbo: this.$('.carbo input').val().trim(),
+				fat: this.$('.fat input').val().trim(),
+				protein: this.$('.protein input').val().trim()
+			});
+
+			this.endEdit();
+		},
+
+		cancel: function () {
+			this.endEdit();
+		},
+
+		endEdit: function () {
+			this.$nutritionFacts.find('.progress').show();
+			this.$nutritionFacts.find('.goals-edit').hide();
+			this.$editBtn.show();
+			this.$cancelBtn.hide();
+			this.$submitBtn.hide();
 		}
 	});
 
